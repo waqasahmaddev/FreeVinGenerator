5e03dcff8eff508000396a232b90b32340c9fe92
Waqas Ahmad <waqas.ahmad6253@gmail.com>
2025-11-18 00:18:30 +0500

    add ready-to-use Gulp tasks for CSS/JS minification

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp      = require('gulp');
+var cleanCSS  = require('gulp-clean-css');
+var uglify    = require('gulp-uglify');
+var rename    = require('gulp-rename');
+
+/* --- Minify CSS --- */
+gulp.task('css', function () {
+    return gulp.src('app.css')
+        .pipe(cleanCSS())
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('./'));
+});
+
+/* --- Minify JS --- */
+gulp.task('js', function () {
+    return gulp.src('app.js')
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('./'));
+});
+
+/* --- Default Task --- */
+gulp.task('default', gulp.parallel('css', 'js'));
